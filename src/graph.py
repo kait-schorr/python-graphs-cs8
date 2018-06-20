08a186be36ae5f77fa9f74bf74c36d209cfb7e71
kait-schorr <kaitlynschorr@gmail.com>
2018-06-20 16:15:00 -0500

    ADDED FOURTH VERTEX & THIRD EDGE -created extra dummy data to test labels, edges, and vertexes

--- src/graph.py
+++ src/graph.py
@@ -19,10 +19,14 @@ class Graph:
         debug_vertex_1 = Vertex("t1", x=10, y=20)
         debug_vertex_2 = Vertex("t2", x=300, y=400)
         debug_vertex_3 = Vertex("t3", x=200, y=250)
+        debug_vertex_4 = Vertex("t4", x=400, y=450)
 
         debug_edge_1 = Edge(debug_vertex_2)
         debug_edge_2 = Edge(debug_vertex_3)
+        debug_edge_3 = Edge(debug_vertex_3)
         debug_vertex_1.edges.append(debug_edge_1)
         debug_vertex_2.edges.append(debug_edge_2)
+        debug_vertex_4.edges.append(debug_edge_3)
 
-        self.vertexes.extend([debug_vertex_1, debug_vertex_2, debug_vertex_3])
+        self.vertexes.extend(
+            [debug_vertex_1, debug_vertex_2, debug_vertex_3, debug_vertex_4])
